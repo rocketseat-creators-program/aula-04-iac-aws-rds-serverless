2a231bef5d3ed8d694870c849f6c0f3396b51d35
Willian da Silva <silva.willian@outlook.com>
2021-08-26 08:34:33 -0300

    Criando as  variaveis locais

---
+++ locals.tf
@@ -0,0 +1,14 @@
+locals {
+  identifier          = "${var.product}-${var.project}-${var.env}-${var.envVersion}"
+  security_group_name = "${random_string.resources_prefix.result}-${var.env}-${var.envVersion}-db-sg"
+  subnet_group_name   = "${var.product}-${var.project}-${var.env}-${var.envVersion}-sg"
+  tags = {
+    "environment"        = var.env
+    "environmentVersion" = var.envVersion
+    "createdBy"          = var.createdBy
+    "owner"              = var.owner
+    "role"               = var.role
+    "project"            = var.project
+    "product"            = var.product
+  }
+}
